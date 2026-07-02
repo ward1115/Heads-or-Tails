cbb8941cd31c7d23b96595443ea0f714fce0b95d
中辻拓馬 <s1330196@u-aizu.ac.jp>
2026-07-02 17:10:00 +0900

    HoT: initial revision

---
+++ hot.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int heads = 0, tails = 0;
+    int i;
+
+    srand(time(NULL));
+    
+    printf("Tossing a coin...\n");
+    for(i=0;i<3;i++){
+        if(rand() % 2 == 0){
+            heads++;
+            printf("Round %d: Heads\n", i+1);
+        } else {
+            tails++;
+            printf("Round %d: Tails\n", i+1);
+        }
+    }
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+
+    return 0;
+}
